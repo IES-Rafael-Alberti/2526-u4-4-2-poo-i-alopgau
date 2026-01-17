c72f0f337bceecec7e71dcd4b55d9aa39e2ecd47
alopgau <alopezg@ieslabahia.es>
2026-01-17 14:06:22 +0100

    Coche hecho

---
+++ src/main/kotlin/org/iesra/CocheMain.kt
@@ -0,0 +1,37 @@
+package org.iesra
+
+fun main() {
+
+val c1 = Coche("Amarillo", "Seat", "ibiza", 200,4, "1101GJB")
+try {
+    val c2 = Coche("Amarillo", "Seat", "Panda", 200,6, "1101GJB")
+    println(c2)
+} catch (e: IllegalArgumentException) {
+    println("Número puertas inválido")
+}
+try {
+    val c3 = Coche("Amarillo", "Seat", "Panda", 200,4, "1101GJBH")
+    println(c3)
+} catch (e: IllegalArgumentException) {
+    println("Matrícula inválida")
+}
+try {
+    val c4 = Coche("Amarillo", "Seat", "Panda", 69,5, "1101GJB")
+    println(c4)
+} catch (e: IllegalArgumentException) {
+    println("Número caballos inválido")
+}
+
+try {
+    val c5 = Coche("Amarillo", "Seat", "Panda", 701,5, "1101GJB")
+    println(c5)
+} catch (e: IllegalArgumentException) {
+    println("Número caballos inválido")
+}
+try {
+    val c6 = Coche("Amarillo", "Seat", "Panda", 701,2, "1101GJB")
+    println(c6)
+} catch (e: IllegalArgumentException) {
+    println("Número puertas inválido")
+}
+}
